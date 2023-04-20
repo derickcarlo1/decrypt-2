cfc974e84fcde2e52e3ba5acc8a5b28655e2b4f9
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-21 06:39:06 +0800

    plotting pseudo, dialog and some msgs to be printed

---
+++ decrypt2.py
@@ -0,0 +1,14 @@
+# Create header
+
+# Define the character substitution dictionary
+
+# Define the decryption function
+
+# Prompt the user for input and decrypt it
+
+"Enter the message to decrypt: "
+
+"Do you want to decrypt another message? (y/n): "
+  
+"Exiting program. Goodbye!"
+"Invalid input, will exit the program. Goodbye!"
